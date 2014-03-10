bbea0634326329702dfec649841afb8d9c1041e9
Daiki UEDA <daikiueda.1979@gmail.com>
2014-03-10 19:05:57 +0900

    関数オブジェクトのtoString()

--- js/Function.js
+++ js/Function.js
@@ -1,10 +1,24 @@
 var expect = chai.expect;
 
-describe( 'Function', function(){
+describe( "Function", function(){
 
-    it( '関数オブジェクトのlengthプロパティで、仮引数の数が取得できる。', function(){
-        expect( ( function(){} ).length ).equal( 0 );
-        expect( ( function( hoge ){} ).length ).equal( 1 );
-        expect( ( function( hoge, foo ){} ).length ).equal( 2 );
+    describe( ".length", function(){
+        it( "関数オブジェクトのlengthプロパティで、仮引数の数が取得できる。", function(){
+            expect( ( function(){} ).length ).equal( 0 );
+            expect( ( function( hoge ){} ).length ).equal( 1 );
+            expect( ( function( hoge, foo ){} ).length ).equal( 2 );
+        } );
+    } );
+
+    describe( ".toString()", function(){
+        // https://github.com/sindresorhus/multiline
+        it( "コメントも含まれる。", function(){
+            expect( ( function(){ /* comment */ } ).toString() ).to.contain( "/* comment */" );
+        } );
+        it( "コメントの改行も含まれる。", function(){
+            expect( ( function(){/* Line1(\n)
+                Line2(\n)
+            */ } ).toString().match( /\n/g ).length ).to.equal( 2 );
+        } );
     } );
 } );

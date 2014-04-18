a488be736828d5919d6f2b7eac39855d91d2338a
daikiueda <daikiueda@users.noreply.github.com>
2014-04-19 00:42:20 +0900

    変数の巻き上げについておさらい。

---
+++ js/Scope.js
@@ -0,0 +1,32 @@
+if( !chai ) var chai = require( "chai" );
+var expect = chai.expect;
+
+describe( "Scope", function(){
+
+    describe( "変数の巻き上げ", function(){
+    	it( "未宣言の変数を参照するとエラーが発生する。", function(){
+    		expect( function(){
+    			return hoge;
+    		} ).to.throw( Error );
+    	} );
+
+    	it( "変数宣言があれば参照はできるが、代入式より前の行では、変数の値はundefinedである。エラーは発生しない。", function(){
+    		expect( ( function(){
+    			return hoge;
+    			var hoge = 1;
+    		} )() ).to.be.an( "undefined" );
+
+    		expect( function(){
+    			return hoge;
+    			var hoge = 1;
+    		} ).not.to.throw( Error );
+    	} );
+
+    	it( "宣言・代入式の後であれば、当然値を取得できる。", function(){
+    		expect( ( function(){
+    			var hoge = 1;
+    			return hoge;
+    		} )() ).to.equal( 1 );
+    	} );
+    } );
+} );
